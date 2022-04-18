e0e1d1e2bc21da736882bca25063eb48949bd39c
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-04-18 13:36:19 -0300

    feat: Shopping Cart React-Js

---
+++ src/Home.js
@@ -0,0 +1,30 @@
+import React from "react";
+import Itemcard from "./Itemcard";
+import data from "./data";
+
+const Home = () => {
+  console.log(data.produtoData);
+
+  return (
+    <>
+      <h1 className="text-center mt-3">Adcionar Nomes</h1>
+      <section className="py-4 container">
+        <div className="row justify-content-center">
+          {data.produtoData.map((item, index) => {
+            return (
+              <Itemcard
+                img={item.img}
+                title={item.title}
+                desc={item.desc}
+                price={item.price}
+                key={index}
+              />
+            );
+          })}
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/data.js
@@ -0,0 +1,43 @@
+import React from "react";
+import img1 from "./img/img1.png";
+import img2 from "./img/img2.png";
+import img3 from "./img/img3.png";
+import img4 from "./img/img4.png";
+
+const data = {
+  produtoData: [
+    {
+      id: 1,
+      img: img1,
+      title: "Melancia",
+      desc: "",
+      price: 10,
+    },
+
+    {
+      id: 2,
+      img: img2,
+      title: "Manga",
+      desc: "",
+      price: 5,
+    },
+
+    {
+      id: 3,
+      img: img3,
+      title: "Melão",
+      desc: "",
+      price: 3,
+    },
+
+    {
+      id: 4,
+      img: img4,
+      title: "Maçã",
+      desc: "",
+      price: 14,
+    },
+  ],
+};
+
+export default data;
