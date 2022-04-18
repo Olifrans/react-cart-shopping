8aca6ef69240e3719b9531ae66eca51d0a73e17e
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-04-18 15:05:00 -0300

    feat: Adcionar ao carrinho

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-
 import React from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Home from "./Home"
+import Cart from "./Cart";
+import Home from "./Home";
+import { CartProvider } from "react-use-cart";
 
 function App() {
   return (
-    <Home />    
+    <>
+      <CartProvider>
+        <Home />
+        <Cart />
+      </CartProvider>
+    </>
   );
 }
 
--- src/Cart.js
+++ src/Cart.js
@@ -1,11 +1,58 @@
-import React from 'react';
+import React from "react";
+import { useCart } from "react-use-cart";
 
 const Cart = () => {
-    return (
-        <div>
-            
+  const {
+    isEmpty,
+    totalUniqueItems,
+    items,
+    totalItems,
+    cartTotal,
+    updateItemQuantity,
+    removeItem,
+    emptyCart,
+  } = useCart();
+
+  if (isEmpty) return <h1 className="text-center">Seu carrinho está vazio</h1>;
+
+  return (
+    <section className="py-4 container">
+      <div className="row justify-content-center">
+        <div className="col-12">
+          {console.warn(items)}
+          <h5>
+            Carrinho({totalUniqueItems}) total Itens: ({totalItems}){" "}
+          </h5>
+
+          <table className="table table-light table-hover m-0">
+            <tbody>
+              {items.map((item, index) => {
+                return (
+                  <tr key={index}>
+                    <td>
+                      <img src={item.img} style={{ height: "6rem" }} />
+                    </td>
+
+                    <td>{item.title}</td>
+                    <td>R$ {item.price}</td>
+                    <td>Quantidade ({item.quantity}) </td>
+
+                    <td>
+                        <button>-</button>
+                        <button>+</button>
+                        <button>Remover Item</button>
+
+                    </td>
+
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
         </div>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default Cart;
