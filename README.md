62447321b4aa47ce713f9c6b340c1c5a0d35b069
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-04-18 16:54:24 -0300

    feat: Foi uma boa diversão

--- README.md
+++ README.md
@@ -13,14 +13,6 @@ yarn add bootstrap
 
 
 
-
-
-
-
 ### Componente Card getbootstrap
 https://getbootstrap.com/docs/5.0/components/card/
 
-
-
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
--- src/Cart.js
+++ src/Cart.js
@@ -11,6 +11,7 @@ const Cart = () => {
     updateItemQuantity,
     removeItem,
     emptyCart,
+    
   } = useCart();
 
   if (isEmpty) return <h1 className="text-center">Seu carrinho está vazio</h1>;
@@ -38,18 +39,40 @@ const Cart = () => {
                     <td>Quantidade ({item.quantity}) </td>
 
                     <td>
-                        <button>-</button>
-                        <button>+</button>
-                        <button>Remover Item</button>
-
+                      <button
+                        className="btn btn-info ms-2"
+                        onClick={() => updateItemQuantity(item.id, item.quantity - 1)}
+                        >-</button>
+                      <button
+                        className="btn btn-info ms-2"
+                        onClick={() => updateItemQuantity(item.id, item.quantity + 1)}
+                        >+</button>
+                      <button 
+                      className="btn btn-danger ms-2"
+                      onClick={() => removeItem(item.id)}
+                      >Remover Item</button>
                     </td>
-
                   </tr>
                 );
               })}
             </tbody>
           </table>
         </div>
+
+        <div className="col-auto ms-auto">
+              <h2>Preço Total: R$ {cartTotal} </h2>
+        </div>
+
+
+        <div className="col-auto">
+        <button className="btn btn-danger ms-2" onClick={() => emptyCart()}>Limpar Carrinho</button>
+
+        <button className="btn btn-primary ms-2">Fazer Pagamento</button>
+
+
+        </div>
+
+
       </div>
     </section>
   );
